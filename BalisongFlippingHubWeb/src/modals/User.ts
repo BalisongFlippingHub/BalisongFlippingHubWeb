9e6c68461a1ab937cdec7ee4fa49e739b518abc5
Tyler Zenisek <tzenisekj@gmail.com>
2024-09-22 22:44:40 -0500

    updated models

--- BalisongFlippingHubWeb/src/modals/User.ts
+++ BalisongFlippingHubWeb/src/modals/User.ts
@@ -1,25 +1,24 @@
-
 export interface Profile {
-    /*All Profiles Data*/
-    id: string | null,
-    email: string | null,  
-    posts: Array<string>,
-    accountCreationDate: Date | null
-    role: string 
+  /*All Profiles Data*/
+  id: string | null;
+  email: string | null;
+  accountCreationDate: Date | null;
+  lastLogin: Date | null;
+  role: string | null;
 
-    /*Profile Data specific for Users*/
-    facebookLink?: string | null,
-    instagramLink?: string | null,
-    twitterLink?: string | null,
-    youtubeLink?: string | null,
-    redditLink?: string | null,
-    discordLink?: string | null,
-    personalEmailLink?: string | null,
-    personalWebsiteLink?: string | null,
+  /*Profile Data specific for Users*/
+  facebookLink?: string | null;
+  instagramLink?: string | null;
+  twitterLink?: string | null;
+  youtubeLink?: string | null;
+  redditLink?: string | null;
+  discordLink?: string | null;
+  personalEmailLink?: string | null;
+  personalWebsiteLink?: string | null;
 
-    profileImg?: string | null,
-    bannerImg?: string | null,
-    displayName?: string | null
+  profileImg?: string | null;
+  bannerImg?: string | null;
+  displayName?: string | null;
 
-    collectionId?: string | null
+  collectionId?: string | null;
 }
