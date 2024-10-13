64768d277da24971c72abb61142b8b6d30869861
Tyler Zenisek <tzenisekj@gmail.com>
2024-10-12 21:05:05 -0500

    updated home page

--- BalisongFlippingHubWeb/src/pages/HomePage.tsx
+++ BalisongFlippingHubWeb/src/pages/HomePage.tsx
@@ -9,58 +9,101 @@ const HomePage = () => {
 
   return (
     <>
-      <section className="h-screen text-lg font-semibold pt-[64px] lg:pl-[192px] flex flex-col relative">
-        
+      <section className="md:h-screen xsm:h-auto text-lg font-semibold pt-[64px] lg:pl-[192px] flex flex-col relative">
+        <div className="w-full md:h-1/2 xsm:h-auto flex md:flex-row xsm:flex-col-reverse">
+          <div className="md:w-1/2 xsm:w-full flex justify-center items-center p-10">
+            <div className="flex flex-col items-center gap-4">
+              <h4 className="text-3xl font-bold">Welcome!</h4>
 
-        <div className="w-full h-1/2"></div>
+              <p className="text-xl text-center">
+                Welcome to the Balisong Flipping Center! The central hub for
+                balisong related content and the home of knife enthusiest,
+                flippers, modders and more. Scroll to learn more, or make an
+                account today to jump right into the balisong community.
+              </p>
 
-        <div className="w-full flex h-1/2 gap-1">
-          <div className="w-1/3 p-4 flex flex-col items-center bg-black">
-            <p className="text-center">
-              Check out what the community is up to here at the community page. The home for all balisong enthusiests posting, awesome flipping clips, montages, or inquires about the hobby. 
-            </p>
+              <button
+                type="button"
+                onClick={() => navigate("/register")}
+                className="p-2 rounded bg-black hover:bg-shadow-green-offset hover:border hover:border-white border border-shadow-green"
+              >
+                Register Now
+              </button>
 
-            <h3 className="mt-6 font-semibold text-2xl">
-            Check out the Community
+              <div className="flex gap-2 items-center">
+                <p>Already have an account?</p>
+                <button
+                  type="button"
+                  onClick={() => navigate("/login")}
+                  className="underline hover:text-blue"
+                >
+                  Login Here
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="md:w-1/2 xsm:w-full md:p-10 xsm:p-0 flex items-center justify-center">
+            <HomePageCaurosel />
+          </div>
+        </div>
+
+        <div className="w-full flex xsm:flex-col md:flex-row md:h-1/2 xsm:h-auto gap-[1px]">
+          {/*Community Page Info Display*/}
+          <div className="md:w-1/3 xsm:w-full p-4 flex flex-col gap-6 items-center justify-center bg-black">
+            <h3 className="mt-6 font-bold text-3xl underline">
+              Check out the Community
             </h3>
 
+            <p className="text-center text-xl">
+              The hub for balisong enthusiest alike to share their knives,
+              update their collections, make posts and support the community.
+            </p>
+
             <button
-            onClick={() => navigate("/community")}
-            className="bg-shadow-green p-3 rounded mt-2 hover:bg-shadow-green-offset"
+              onClick={() => navigate("/community")}
+              className="bg-shadow-green p-3 rounded mt-2 hover:bg-shadow-green-offset hover:border border border-black hover:border-white text-2xl font-bold"
             >
-            Community
-            <FontAwesomeIcon icon={faGlobe} className="ml-2" />
+              Community
+              <FontAwesomeIcon icon={faGlobe} className="ml-2" />
             </button>
           </div>
 
-          <div className="w-1/3 flex flex-col items-center p-4 bg-black">
-            <p className="text-center">
-              Looking to start your flipping career or improve your skills
-              with new tricks with tutorials and slo-mo's?
-            </p>
-            <h3 className="mt-6 font-semibold text-lg">
+          {/*Tutorial Center Info Display*/}
+          <div className="md:w-1/3 xsm:w-full p-4 flex flex-col gap-6 items-center justify-center bg-black">
+            <h3 className="mt-6 font-bold text-3xl underline">
               Check out the Tutorial Center
             </h3>
+
+            <p className="text-center text-xl">
+              For new comers and professionals. Check out the basics, learn new
+              tricks, or find inspiration from some of the best in the world.
+            </p>
+
             <button
               onClick={() => navigate("/tutorial-center")}
-              className="bg-shadow-green p-3 rounded mt-2 hover:bg-shadow-green-offset"
+              className="bg-shadow-green p-3 rounded mt-2 hover:bg-shadow-green-offset hover:border border border-black hover:border-white text-2xl font-bold"
             >
               Tutorial Center
               <FontAwesomeIcon icon={faHubspot} className="ml-2" />
             </button>
           </div>
 
-          <div className="w-1/3 flex flex-col items-center p-4 bg-black">
-            <p className="text-center">
-              Want to check out the available prodcucts, biggest makers, or
-              looking for info on a specific knife?{" "}
-            </p>
-            <h3 className="mt-6 font-semibold text-lg">
+          {/*Product World Info Display*/}
+          <div className="md:w-1/3 xsm:w-full p-4 flex flex-col gap-6 items-center justify-center bg-black">
+            <h3 className="mt-6 font-bold text-3xl underline">
               Check out the Product World
             </h3>
+
+            <p className="text-center text-xl">
+              The informational hub for modders, knife makers, products and
+              more. Check out the newest in the industry, or information on past
+              products.
+            </p>
+
             <button
               onClick={() => navigate("/product-world")}
-              className="bg-shadow-green p-3 rounded mt-2 hover:bg-shadow-green-offset"
+              className="bg-shadow-green p-3 rounded mt-2 hover:bg-shadow-green-offset hover:border border border-black hover:border-white text-2xl font-bold"
             >
               Product World
               <FontAwesomeIcon icon={faEarthAmericas} className="ml-2" />
