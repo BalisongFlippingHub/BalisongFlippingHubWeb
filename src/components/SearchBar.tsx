475bf37d2998f1476ae33a277af927986fc65697
Tyler Zenisek <tzenisekj@gmail.com>
2025-01-23 22:41:51 -0600

    changed search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,35 +3,44 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 interface params {
-    toggleSearchBar: Function
+  toggleSearchBar: Function;
 }
 
 const SearchBar = ({ toggleSearchBar }: params) => {
-    const [showResults, toggleResults] = useState(false)
+  const [showResults, toggleResults] = useState(false);
 
-    const handleOnChange = () => {
-        toggleResults(true)
-    }
+  const handleOnChange = () => {
+    toggleResults(true);
+  };
 
-    return (
-        <div className="relative">
-            <div className="flex items-center w-96">
-                <input placeholder="Search..." className="p-2 w-full bg-shadow-green-offset" onChange={handleOnChange}/>
-                <FontAwesomeIcon icon={faMagnifyingGlass} className="p-2 h-7 hover:cursor-pointer" />
-                <button className="md:collapse ml-2 text-lg hover:cursor-pointer" type="button" onClick={() => toggleSearchBar()}>X</button>
-            </div>
+  return (
+    <div className="relative">
+      <div className="flex items-center w-full">
+        <input
+          placeholder="Search..."
+          className="p-2 w-full bg-shadow-green-offset"
+          onChange={handleOnChange}
+        />
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className="p-2 h-7 hover:cursor-pointer"
+        />
+        <button
+          className="md:collapse ml-2 text-lg hover:cursor-pointer"
+          type="button"
+          onClick={() => toggleSearchBar()}
+        >
+          X
+        </button>
+      </div>
 
-            {
-                !showResults
-                ?
-                <></>
-                :
-                <div className="absolute bg-white h-10 w-full">
-
-                </div>
-            }
-        </div>
-    )
-}
+      {!showResults ? (
+        <></>
+      ) : (
+        <div className="absolute bg-white h-10 w-full"></div>
+      )}
+    </div>
+  );
+};
 
 export default SearchBar;
