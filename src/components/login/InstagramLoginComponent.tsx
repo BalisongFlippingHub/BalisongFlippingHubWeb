ad4d398c9ab407a7259eddd830e299f1917deb00
tzenisekj <tzenisekj@gmail.com>
2025-04-04 11:03:26 -0500

    fixed build

--- src/components/login/InstagramLoginComponent.tsx
+++ src/components/login/InstagramLoginComponent.tsx
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const InstagramLoginComponent = () => {
     
     return (
-        <button className="flex items-center bg-instagram-pink justify-between gap-2 p-1 hover:cursor-pointer hover:translate-x-4 hover:-translate-y-2 transition duration-150 ease-in-out rounded text-xl" type="button" onClick={() => loginWithGoogle()}>
+        <button className="flex items-center bg-instagram-pink justify-between gap-2 p-1 hover:cursor-pointer hover:translate-x-4 hover:-translate-y-2 transition duration-150 ease-in-out rounded text-xl" type="button">
             <div className="p-1 pr-2 pl-2">
                 <FontAwesomeIcon icon={faInstagram} style={{ color: "white",}} size="xl"/>
             </div>
